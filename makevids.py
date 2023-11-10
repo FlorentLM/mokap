88c6944670766f9476443dedab5ab03a75a6541b
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2023-11-10 13:46:08 +0000

    Updated file names handling

--- makevids.py
+++ makevids.py
@@ -2,10 +2,24 @@
 
 from mokap.files_op import *
 
+##
+
+# if __name__ == '__main__':
+#     for folder in data_folder.iterdir():
+#         # print(folder)
+#         videos_from_zarr(folder)
+
+##
+
+# if __name__ == '__main__':
+#     for folder in data_folder.iterdir():
+#         # print(folder)
+#         convert_videos(folder, filter='avi')
+
+##
+
 if __name__ == '__main__':
     for folder in data_folder.iterdir():
         print(folder)
-        filepath = folder
-        videos_from_zarr(folder)
+        update_names(folder)
 
-##
--- mokap/files_op.py
+++ mokap/files_op.py
@@ -8,12 +8,14 @@ import shutil
 import shlex
 
 ##
+#
+# videos_folder = Path('/mnt/data/Videos')
+# data_folder = Path('/mnt/data/RawFrames')
 
-videos_folder = Path('/mnt/data/Videos')
-data_folder = Path('/mnt/data/RawFrames')
+videos_folder = Path('D:\\Videos')
+data_folder = Path('D:\\RawFrames')
 
 ##
-
 
 COMPRESSED = {'codec': 'libx265',
                 'params': '-crf 1 -preset veryfast',
@@ -31,19 +33,36 @@ DEFAULT_FMT = COMPRESSED
 
 ##
 
+def exists_check(path):
+    """ Checks if a file or folder of the given name exists. If so, created a suffixed version of the name
+    in a smart way. Returns the new, safe to use, name. """
+    i = 2
+    while path.exists():
+
+        if bool(re.match('.+_[0-9]+$', path.stem)):
+            # ends with a '_X' number so let's check if there is also a non-suffixed sibling
+            parts = path.stem.split('_')
+            original_name = ('_').join(parts[:-1])
+            suffix = int(parts[-1])
+
+            if (path.parent / f"{original_name}{path.suffix}").exists():
+                new_name = f"{original_name}_{suffix + 1}{path.suffix}"
+            else:
+                new_name = f"{path.stem}_{i}{path.suffix}"
+        else:
+            new_name = f"{path.stem}_{i}{path.suffix}"
+
+        path = path.parent / new_name
+        i += 1
+    return path
+
 def mk_folder(name=''):
 
     if name == '':
         name = datetime.now().strftime('%y%m%d-%H%M')
 
-    new_path = data_folder / name
+    new_path = exists_check(data_folder / name)
 
-    i = 1
-    while new_path.exists():
-        name = new_path.stem.split('_')[0]
-        incremented = name + f"_{i}"
-        new_path = new_path.parent / incremented
-        i += 1
     new_path.mkdir(parents=True, exist_ok=False)
     return new_path
 
@@ -113,7 +132,6 @@ def videos_from_zarr(filepath, cams=None, format=None, force=False):
     else:
         cams = set(range(nb_cams))
 
-    conv_settings = DEFAULT_FMT
     if format is not None:
         if 'compr' in str(format):
             conv_settings = COMPRESSED
@@ -123,6 +141,8 @@ def videos_from_zarr(filepath, cams=None, format=None, force=False):
             conv_settings = LOSSLESS_2
         else:
             conv_settings = DEFAULT_FMT
+    else:
+        conv_settings = DEFAULT_FMT
 
     for c in cams:
 
@@ -135,7 +155,7 @@ def videos_from_zarr(filepath, cams=None, format=None, force=False):
             start, end = root['times'][s]
             delta += end - start
 
-        total_time_s = delta / np.timedelta64(1, 's')
+        total_time_s = delta / int(np.timedelta64(1, 's'))
 
         fps_calc = nb_frames/total_time_s
 
@@ -145,11 +165,11 @@ def videos_from_zarr(filepath, cams=None, format=None, force=False):
               f"Actual (mean):\n  {fps_calc:.2f} fps\n"
               f"--> Error = {(1-(fps_calc/fps_raw))*100:.2f}%")
 
-        outname = f'vid_cam{c}_sess{s}.{conv_settings["ftype"]}'
-
         outfolder = videos_folder / filepath.stem
         if not outfolder.exists():
             outfolder.mkdir(parents=True, exist_ok=False)
+
+        outname = f'{outfolder.stem}_cam{c}.{conv_settings["ftype"]}'
 
         savepath = outfolder / outname
 
@@ -168,7 +188,7 @@ def videos_from_zarr(filepath, cams=None, format=None, force=False):
 
         print('Done.')
 
-def convert_videos(path, output_format=None, extension=''):
+def convert_videos(path, filter='*', output_format=None, delete_source=False):
 
     path = Path(path)
 
@@ -176,10 +196,9 @@ def convert_videos(path, output_format=None, extension=''):
         to_convert = [path]
         ftype = 'file'
     else:
-        to_convert = list(path.glob(f'*.{extension.strip(".").lower()}'))
+        to_convert = list(path.glob(filter))
         ftype = 'folder contents'
 
-    conv_settings = DEFAULT_FMT
     if output_format is not None:
         if 'compr' in str(output_format):
             conv_settings = COMPRESSED
@@ -189,15 +208,52 @@ def convert_videos(path, output_format=None, extension=''):
             conv_settings = LOSSLESS_2
         else:
             conv_settings = DEFAULT_FMT
+    else:
+        conv_settings = DEFAULT_FMT
 
     for f in to_convert:
 
         output_name = f.stem + f'.{conv_settings["ftype"]}'
         savepath = f.parent / output_name
 
+        if output_name == f.name:
+            f = f.rename(f.parent / f'{f.stem}_orig{f.suffix}')
+
         process = sp.Popen(shlex.split(f'ffmpeg -i {f.as_posix()} -c:v {conv_settings["codec"]} {conv_settings["params"]} -pix_fmt gray -an {savepath.as_posix()}'))
 
         process.wait()
         process.terminate()
 
+        if delete_source:
+            if savepath.is_file() and savepath.stat().st_size > 0:
+                f.unlink(missing_ok=True)
+
     print(f'Done converting {path.name} {ftype}.')
+
+
+def update_names(path, filter='*'):
+
+    path = Path(path)
+
+    if path.is_file():
+        source_names = [path]
+        ftype = 'file'
+    else:
+        source_names = list(path.glob(filter))
+        ftype = 'folder contents'
+
+    any_renamed = False
+    for f in source_names:
+
+        if bool(re.search('vid_cam[0-9+]_sess[0-9+]', f.stem)):
+
+            new_name = f"{f.parent.stem}_{f.stem.split('_')[1]}{f.suffix}"
+            savepath = exists_check(f.parent / new_name)
+
+            f.rename(savepath)
+            any_renamed = True
+
+    if any_renamed:
+        print(f'Done renaming {path.name} {ftype}.')
+    else:
+        print(f'Nothing needed to be renamed.')
