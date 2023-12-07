b2bb0e664cdf0b9db4eae2e51db528d2ecd3b187
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2023-12-07 13:54:23 +0000

    Testing motion detection

--- some_tests/detection_test.py
+++ some_tests/detection_test.py
@@ -1,21 +1,68 @@
-from mokap.core import Manager, MotionDetector, cv2
+from mokap.core import Manager, MotionDetector
+from sys import platform
+import subprocess
+import cv2
+
+def get_webcam_devices():
+
+    if platform == "linux" or platform == "linux2":
+        result = subprocess.run(["ls", "/dev/"],
+                                 stdout=subprocess.PIPE,
+                                 text=True)
+        devices = [int(v.replace('video', '')) for v in result.stdout.split() if 'video' in v]
+    elif platform == "darwin":
+        print('macOS: TODO')
+    elif platform == "win32":
+        print('macOS: TODO')
+    else:
+        raise OSError('Unsupported OS')
+
+    working_ports = []
+
+    prev_log_level = cv2.setLogLevel(0)
+
+    for dev in devices:
+
+        cap = cv2.VideoCapture(dev)
+
+        if cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                working_ports.append(dev)
+
+    cv2.setLogLevel(prev_log_level)
+    return working_ports
+
+
+##
 
 md = MotionDetector()
 
-mgr = Manager()
-mgr.connect()
-mgr.exposure = 15000
-mgr.framerate = 10
-mgr.binning = 2
-mgr.on()
+use_webcam = True
+
+if use_webcam:
+    available_cameras = get_webcam_devices()
+    cap = cv2.VideoCapture(available_cameras[0])
+else:
+    mgr = Manager()
+    mgr.connect()
+    mgr.exposure = 15000
+    mgr.framerate = 10
+    mgr.binning = 2
+    mgr.on()
 
 cv2.namedWindow('Video')
 cv2.namedWindow('Detection')
 
+
 dims = None
 while True:
 
-    frame = mgr.get_current_framearray(0)
+    if use_webcam:
+        _, frame = cap.read()
+    else:
+        frame = mgr.get_current_framearray(0)
+
     if dims is None:
         dims = frame.shape[0] * frame.shape[1]
 
@@ -37,4 +84,8 @@ while True:
         break
 
 cv2. destroyAllWindows()
-mgr.off()
+
+if use_webcam:
+    cap.release()
+else:
+    mgr.off()
