91f4f53a018f585a7819b6e15167fc03d0f646de
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2024-03-04 19:09:06 +0000

    Added Black level setting to camera

--- mokap/hardware.py
+++ mokap/hardware.py
@@ -200,6 +200,7 @@ class Camera:
 
         self._framerate = framerate
         self._exposure = exposure
+        self._blacklvl = 0.0
         self._gain = 1.0
         self._gamma = 1.0
         self._triggered = triggered
@@ -279,16 +280,22 @@ class Camera:
                 self.ptr.TriggerMode = "Off"
                 self.ptr.AcquisitionFrameRateEnable.SetValue(True)
 
-        if self._is_virtual:
-            self._width = 920
-            self._height = 540
+        if not self._is_virtual:
+            self._width = self.ptr.WidthMax.GetValue()
+            self._height = self.ptr.HeightMax.GetValue()
+        else:
+            # We hardcode these for virtual cameras, because the virtual sensor is otherwise 4096x4096 px...
+            self._width = 1440
+            self._height = 1080
+            self.ptr.Width = self._width
+            self.ptr.Height = self._height
 
-        # Apply binning - THIS ALSO SETS the width and height
         self.binning = self._binning
         self.binning_mode = self._binning_mode
 
         self.framerate = self._framerate
         self.exposure = self._exposure
+        self.blacklvl = self._blacklvl
         self.gain = self._gain
         self.gamma = self._gamma
 
@@ -374,6 +381,10 @@ class Camera:
         return self._exposure
 
     @property
+    def blacklvl(self) -> float:
+        return self._blacklvl
+
+    @property
     def gain(self) -> float:
         return self._gain
 
@@ -397,18 +408,13 @@ class Camera:
             self.ptr.BinningHorizontal.SetValue(value)
 
             if not self._is_virtual:
-                # Actual frame size
+                # Compute actual frame size
                 self._width = self.ptr.SensorWidth.GetValue() // value
                 self._height = self.ptr.SensorHeight.GetValue() // value
             else:
-                self._width = self._width // value
-                self._height = self._height // value
-
-            # Set ROI to full frame and centre it
-            self.ptr.Width = self._width
-            self.ptr.Height = self._height
-            self.ptr.CenterX = True
-            self.ptr.CenterY = True
+                # We hardcode these for virtual cameras, because the virtual sensor is otherwise 4096x4096 px...
+                self._width = 1440 // value
+                self._height = 1080 // value
 
         # And keep a local value to avoid querying the camera every time we read it
         self._binning = value
@@ -441,6 +447,13 @@ class Camera:
 
         # And keep a local value to avoid querying the camera every time we read it
         self._exposure = value
+
+    @blacklvl.setter
+    def blacklvl(self, value: float) -> None:
+        if self._connected:
+            self.ptr.BlackLevel.SetValue(value)
+        # And keep a local value to avoid querying the camera every time we read it
+        self._blacklvl = value
 
     @gain.setter
     def gain(self, value: float) -> None:
