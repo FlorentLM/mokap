9cb513991369a6be704582029d5c960b1cd36546
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2024-07-02 18:44:08 +0100

    Cleaned main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from mokap.core import Manager
 mgr = Manager(config='./config.yaml', triggered=True, silent=False)
 
 # Set exposure for all cameras (in µs)
-# (maximum exposure time for maximum framerate is 4318 µs)
-# mgr.exposure = 15000
 mgr.exposure = 5000
 
 # Enable binning
@@ -14,12 +12,10 @@ mgr.binning = 1
 mgr.binning_mode = 'avg'
 
 # Set framerate in images per second
-# mgr.framerate = 60
 mgr.framerate = 80
 
 mgr.gamma = 1.0
 mgr.blacks = 1.0
-# mgr.gain = 8.5
 mgr.gain = 0.0
 
 if __name__ == '__main__':
