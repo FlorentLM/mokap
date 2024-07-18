732cc2825772528628e976f90aa2281f94d2aec0
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2024-07-18 14:40:36 +0100

    Cleaned up

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
+
 from mokap.core import MultiCam
-from mokap.interface_qt import *
+from mokap.interface import *
 
 mc = MultiCam(config='./config.yaml', triggered=False, silent=False)
 
-# Set exposure for all cameras (in µs)
+# Example:
+# Set some default parameters for all cameras at once
+
 mc.exposure = 4800
-
-# Enable binning
-mc.binning = 1
-mc.binning_mode = 'avg'
-
-# Set framerate in images per second for all cameras at once
 mc.framerate = 100
-
 mc.gamma = 1.0
 mc.blacks = 1.0
 mc.gain = 0.0
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     if mc.nb_cameras == 0:
         exit()
+
     main_window = MainWindow(mc)
     main_window.show()
 
