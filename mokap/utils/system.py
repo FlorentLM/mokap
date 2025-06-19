47efcc1ed0bd5d07da5a63b231169ce3bf0aa6b6
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2025-06-19 12:48:58 +0100

    Removed a bunch of unused code (vulture)

--- mokap/utils/system.py
+++ mokap/utils/system.py
@@ -41,40 +41,6 @@ def setup_ulimit(wanted_value=8192, silent=True):
             print(f'[INFO] Current file descriptors limit seems fine (n={current_limit})')
 
 
-# TODO: Use this in a GUI helper to populate settings
-def get_encoders(ffmpeg_path='ffmpeg', codec='hevc'):
-    """
-    Get available encoders for the given codec. This is tailored for h264 and hevc (h265)
-    but this may work with others
-    """
-    if '265' in codec:
-        codec = 'hevc'
-    elif '264' in codec:
-        codec = 'h264'
-
-    r = check_output([ffmpeg_path, '-hide_banner', '-codecs'], stderr=False).decode('UTF-8').splitlines()
-    codec_line = list(filter(lambda x: codec in x, r))[0]
-
-    all_encoders = codec_line.split('encoders: ')[1].strip(')').split()
-
-    encoders_names = []
-
-    for encoder in all_encoders:
-        r = check_output([ffmpeg_path, '-hide_banner', '-h', f'encoder={encoder}'], stderr=False).decode('UTF-8').splitlines()
-        name_line = list(filter(lambda x: f'Encoder {encoder}' in x, r))[0]
-        true_name = name_line.split(f'Encoder {encoder} [')[1][:-2]
-        encoders_names.append(true_name)
-
-    unique_encoders = {}
-    for a, b in zip(all_encoders, encoders_names):
-        if b not in unique_encoders:
-            unique_encoders[b] = a
-    return list(unique_encoders.values())
-
-# TODO: We should also query the supported presets and options and auto adjust ...
-
-
-
 # TODO: These probably should be exposed as an advanced tool in the GUI
 def enable_usb(hub_number):
     """
